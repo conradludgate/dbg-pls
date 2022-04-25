ebf5adf77f93a91e0506f4476e1418a13eee7f41
Conrad Ludgate <conrad.ludgate@truelayer.com>
2022-04-25 11:50:40 +0100

    move

---
+++ tests/syn.rs
@@ -0,0 +1,39 @@
+macro_rules! assert_pretty_syn_snapshot {
+    ($ty:ty => $code:literal) => {
+        let parsed = &syn::parse_str::<$ty>($code).unwrap();
+        let format = format!(
+            "pretty:\n{}\n\ncolor:\n{}\n",
+            dbg_pls::pretty(&parsed),
+            dbg_pls::color(&parsed)
+        );
+        insta::assert_snapshot!(insta::_macro_support::AutoName, &format, $code);
+    };
+}
+
+#[test]
+fn expr_array() {
+    assert_pretty_syn_snapshot!(syn::Expr => r#"
+            [
+                "Hello, World! I am a long string",
+                420,
+                "Wait, you can't mix and match types in arrays, is this python?",
+                69,
+                "Nice."
+            ]
+        "#);
+}
+
+#[test]
+fn item_macro() {
+    assert_pretty_syn_snapshot!(syn::Item => r#"
+            macro_rules! assert_pretty_syn_snapshot {
+                ($ty:ty => $code:literal) => {
+                    insta::assert_snapshot!(
+                        insta::_macro_support::AutoName,
+                        &crate::pretty(&syn::parse_str::<$ty>($code).unwrap()).to_string(),
+                        $code
+                    );
+                };
+            }
+        "#);
+}
