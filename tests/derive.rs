ba09334421073fbc8bd0a92f7bde7dabcf9172dd
Daniel Henry-Mantilla <daniel.henry.mantilla@gmail.com>
2023-05-13 19:01:56 +0200

    Add a regression test against raw identifiers

--- tests/derive.rs
+++ tests/derive.rs
@@ -192,3 +192,17 @@ fn ranges() {
 
 #[derive(DebugPls)]
 enum EmptyEnum {}
+
+/// Test raw identifiers.
+#[test]
+fn raw_idents() {
+    #![allow(nonstandard_style)]
+    type r#type = ();
+
+    #[derive(DebugPls)]
+    pub enum r#enum {
+        r#struct { r#fn: r#type },
+    }
+
+    _ = ::dbg_pls::pretty(&r#enum::r#struct { r#fn: () }).to_string();
+}
